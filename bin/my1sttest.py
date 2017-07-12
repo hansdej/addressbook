0653c5dd5a7a5aa565d23d5b021569af95d65393
Hans de Jonge <j.dejonge@gmail.com>
2017-07-12 22:20:50 +0200

    minor fixes

--- bin/my1sttest.py
+++ bin/my1sttest.py
@@ -6,13 +6,10 @@ logging.basicConfig(
         level = logging.INFO)
 
 ab = addressbook.Addressbook()
-c1 = addressbook.Contact('John', 'Doe')
-c2 = addressbook.Contact('Jahn', 'Doe')
-c3 = addressbook.Contact('Jane', 'Doe')
+ab += addressbook.Contact('John', 'Doe')
+ab += addressbook.Contact('Jahn', 'Doe')
+ab += addressbook.Contact('Jane', 'Doe')
 
-ab.add_contact(c1)
-ab.add_contact(c2)
-ab.add_contact(c3)
 ab.del_contact('Jane', 'Doe')
 
 print(ab)
